c3b3532df118299ec86f67ba9752af62c5ed7797
Seungyeon Song <song99pro@gmail.com>
2019-05-28 11:38:06 +0900

    #2 온클릭 이벤트 코드 구현(실행, 멈춤, 리셋)

--- ProgressBar/Form1.cs
+++ ProgressBar/Form1.cs
@@ -12,9 +12,48 @@ namespace ProgressBar
 {
     public partial class Form1 : Form
     {
+
+        int Num = 0;
+        string OrgStr = "";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblStatus.Text;
+
+        }
+
+        private void BtnRun_Click(object sender, EventArgs e)
+        {
+            this.timer.Enabled = true;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.Num++;
+            if(this.Num > 100)
+            {
+                this.timer.Enabled = false;
+                return;
+            }
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr + Num + "%";
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            this.Num = 0;
+            this.pbStatus.Value = Num;
+            this.lblStatus.Text = OrgStr;
+        }
+
+        private void BtnEnd_Click(object sender, EventArgs e)
+        {
+            this.timer.Enabled = false;
         }
     }
 }
